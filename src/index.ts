44bd6e0529f41b725c50adb5448980c62d7b0fe4
kdd9512 <hyunwoo8805@gmail.com>
2022-12-03 23:59:32 +0900

    20221203 3rd commit
    tsconfig.json 설정 및 .d.ts 확장자에 대한 학습(1).

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-class Block {
-    constructor(private data:string) {}
-    static hello() {
-        return "hi";
-    }
-}
+// import 하는것 까진 좋은데 TS 가 이게 뭔질 알아먹질 못한다.
+// TS 에게 해당 파일의 메서드 정의를 설명하기 위해 정의파일을 만들어야 하는데,
+// 해당 파일은 "파일명.d.ts" 확장자를 가진다.
+
+import {init, exit} from "myPackage";
 
---
+++ src/myPackage.d.ts
@@ -0,0 +1,12 @@
+// 어떤 파일을 import 할 때, 그 파일에 첨부된 메서드 등의 파일타입을 정의한다.
+// TS 에게 제시하는 일종의 설명서 같은 것.
+// ** 이 정의가 없다면 TS 는 해당 파일을 인식하지 못한다.
+
+interface Config {
+    url: string;
+}
+
+declare module "myPackage" {
+    function init(config: Config) :boolean;
+    function exit(code:number) :number;
+}
