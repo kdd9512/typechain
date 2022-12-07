f3e0995963b303f59054a26725c3e125f4a0a63c
kdd9512 <hyunwoo8805@gmail.com>
2022-12-07 23:47:19 +0900

    20221207 7th commit
    npm 내 모든 dependency 들의 .d.ts 파일(TS 가 JS 의존성을 이용하기 위해 필수)을
    업로드하고 있는 DefinitelyTyped 이용법 작성.

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,17 @@
 // import {init, exit} from "./myPackage";
 
 // * as : 해당 import 의 모든 구성요소를 불러온다.
+
+// TS 가 어떤 파일 을 import 하여 사용하려면 그 파일이 가지고 있는 메서드들의
+// param, attributes, return 값에 대한 타입 설명이 필요하다.
+// 하지만 import 하는 파일 자체가 거대하다면 이를 하나 하나 설명하는 것은 매우 비효율적인 일이다.
+// 그러므로, 보통은 직접 d.ts 파일을 만들어 설정하기 보다는 DefinitelyTyped 라는 github repository 를 이용한다.
+// 해당 사이트에서 원하는 npm 파일을 찾아 복붙하여 만들수도 있겠으나, 매우 번거로우니
+// 일반적으로 이하의 명령어를 통해 해당 파일을 다운로드 하여 사용하는 방법을 사용한다.
+
+// npm i -D @types/"해당 dependency 를 설명한 d.ts 가 담긴 repository 경로"
+// 이 예제에서 사용하는 crypto 는 nodejs 에 있는 class 이므로 DefinitelyTyped 내 해당 경로인 node 를 선택한다.
+
 import crypto from "crypto";
 
 // 블록체인의 기초를 학습.
@@ -29,6 +40,7 @@ class Block implements BlockShape {
     }
     static calculateHash(prevHash:string, height:number, data:string) {
         const toHash = `${prevHash}${height}${data}`;
+        return crypto.createHash("sha256").update(toHash).digest("hex");
     }
 
 }
